c82ca264e58f3ccaf92a997ab1d564a9225bc819
John Speare <jtspeare@uwaterloo.ca>
2025-07-29 14:16:12 -0400

    Add tests for trie.py

--- tests/test_trie.py
+++ tests/test_trie.py
@@ -10,7 +10,18 @@ from anagrams.trie import Trie
 class Test_Trie(unittest.TestCase):
     """Test class for Trie."""
     def setUp(self):
-        pass
+        self.trie = Trie()
+        self.trie.add("test1")
+        self.trie.add("test2")
 
     def test_add(self):
-        assert True
+        self.assertFalse(self.trie.contains("test3"))
+        self.trie.add("test3")
+        self.assertTrue(self.trie.contains("test3"))
+
+    def test_remove(self):
+        self.assertTrue(self.trie.contains("test1"))
+        self.trie.remove("test1")
+        self.assertFalse(self.trie.contains("test1"))
+
+    
