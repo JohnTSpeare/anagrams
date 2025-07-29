965d47805fca59988acfc4fd07c3b684be7a5723
John Speare <jtspeare@uwaterloo.ca>
2025-07-29 11:40:40 -0400

    Add Dockerfile and empty test modules. Add make test support

---
+++ tests/test_trie.py
@@ -0,0 +1,16 @@
+"""Test module for Trie."""
+import unittest
+
+import mock
+import pytest
+
+from anagrams.trie import Trie
+
+
+class Test_Trie(unittest.TestCase):
+    """Test class for Trie."""
+    def setUp(self):
+        pass
+
+    def test_add(self):
+        assert True
